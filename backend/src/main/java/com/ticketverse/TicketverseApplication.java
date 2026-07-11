74dfd9384b47b61b08f45d651c1e5c4359ee3024
Varnikumar Patel <varnikumar1512@gmail.com>
2026-07-11 19:53:17 +0530

    initialize Spring Boot backend and configure PostgreSQL with Flyway

---
+++ backend/src/main/java/com/ticketverse/TicketverseApplication.java
@@ -0,0 +1,13 @@
+package com.ticketverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketverseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ticketverse/TicketverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
